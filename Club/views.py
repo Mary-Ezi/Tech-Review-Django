2b18a96ffbe7fa8d63fad59ff43a0cb0116624cf
mary-ezi <maryamsadat.eyvazi@seattlecolleges.edu>
2021-02-12 14:57:37 -0800

    Second Resource View commit

--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index (request):
     return render(request, 'Club/index.html')
+
+def Resources(request):
+    Resource_list=Resource.objects.all()
+    return render(request, 'Club/Resource.html', {'Resource_list': Resource_list})
